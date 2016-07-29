beecb8f37fe97b1dec1a2aeb28a5f94422c69180
T.J. Stone <headlesschicken.tj@gmail.com>
2016-07-28 22:47:10 -0700

    Initial Commit

---
+++ ScoresheetTests/ScoresheetTests.swift
@@ -0,0 +1,36 @@
+//
+//  ScoresheetTests.swift
+//  ScoresheetTests
+//
+//  Created by Trevor J. Stone on 7/28/16.
+//  Copyright © 2016 Trevor J. Stone. All rights reserved.
+//
+
+import XCTest
+@testable import Scoresheet
+
+class ScoresheetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
