63b844210de7480f2b52d74849a3020e972f7d2b
T.J. Stone <headlesschicken.tj@gmail.com>
2016-09-06 17:39:18 -0700

    Decreased the zoom factor constant.

--- Scoresheet/Constants.swift
+++ Scoresheet/Constants.swift
@@ -12,7 +12,7 @@ import UIKit
 class Constants: NSObject {
     let GAME_ID_LENGTH: Int = 6
     
-    let ZOOM_FACTOR: CGFloat = 280.0
+    let ZOOM_FACTOR: CGFloat = 130.0
     
     let UPLOAD_URL: String = "http://stats.caha.timetoscore.com/submit-scoresheet.php"
     
